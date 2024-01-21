826eacd6613c0017df0d92a3b487f3eeaa9b32ae
vladislavich245 <150582843+vladislavich245@users.noreply.github.com>
2024-01-21 11:30:06 +0200

    Update text.py

--- text.py
+++ text.py
@@ -7,3 +7,6 @@ question1= input('Хто написав ці рядки?')
 with open(
     'notes.txt','a', encoding='utf-8'
     ) as file:
+    file.write(question1)
+question2 = input("Хочете добавити цитату?(Так/Ні)")
+if question2=='так' or question2=='Так':
